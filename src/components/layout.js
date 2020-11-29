2fc34989329fc2f5349c8e7755db9ad607dd1476
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-11-29 11:55:05 +0000

    Commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,40 +1,14 @@
-
-
-import React from "react"
-import PropTypes from "prop-types"
-
-
-import Header from "./header"
-import "./layout.css"
+import React from 'react';
+import Header from './header';
 
 const Layout = ({ children }) => {
-  
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer style={{
-          marginTop: `2rem`
-        }}>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+      <Header />
+
+      <main>{children}</main>
     </>
-  )
-}
+  );
+};
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
+export default Layout;
