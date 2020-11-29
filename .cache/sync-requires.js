fc7bf4d10d54bc513d20f594254436a4e8e080a0
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-11-29 11:52:13 +0000

    Commit

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -8,7 +8,6 @@ exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/Mohamed/Projects/Travel-Website/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/Mohamed/Projects/Travel-Website/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/Mohamed/Projects/Travel-Website/src/pages/index.js"))),
-  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/Mohamed/Projects/Travel-Website/src/pages/page-2.js"))),
-  "component---src-pages-using-typescript-tsx": hot(preferDefault(require("/Users/Mohamed/Projects/Travel-Website/src/pages/using-typescript.tsx")))
+  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/Mohamed/Projects/Travel-Website/src/pages/page-2.js")))
 }
 
