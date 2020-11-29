169a7fe5a0437750b90d87579675505064d2cec7
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-11-29 11:50:19 +0000

    Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Mo-Travel`,
+    description: `My travel website showcasing the best destinations and deals`,
+    author: `Mohamed Mohamud`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
