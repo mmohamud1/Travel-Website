bf3bb0e270337d6a8e0bae6053fe6c3f5b189f24
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-11-30 14:12:13 +0000

    Commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,25 @@
-import React from "react"
-import {Link} from 'gatsby'
+import React from 'react';
+import { Link } from 'gatsby';
 import styled from 'styled-components';
 
-
 const Header = () => {
-    return (
-        <Nav>
-            <NavLink to='/'>Mo-Travel</NavLink>
-        </Nav>
-    )
-}
-    
+  return (
+    <Nav>
+      <NavLink to='/'>Mo-Travel</NavLink>
+    </Nav>
+  );
+};
 
-
-
-export default Header
+export default Header;
 
 const Nav = styled.nav`
-    background: transparent;
-    height: 80px;
-`
+  background: red;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem calc((100vw - 1300px) / 2);
+  z-index: 100;
+  position: relative;
+`;
 
-const NavLink = styled(Link)``
+const NavLink = styled(Link)``;
