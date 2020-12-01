d28b020fa431d38e569b8c2b129e4d843b5e40a3
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-01 12:16:00 +0000

    Commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Button } from './Button'
 
 const Hero = () => {
     return (
@@ -7,8 +8,39 @@ const Hero = () => {
             <HeroBg>
                 <VideoBg src="" type="video/mp4" />
             </HeroBg>
+            <HeroContent>
+                <HeroItems>
+                    <HeroH1>Unreal Desitinatiions</HeroH1>
+                    <HeroP>Out of this world</HeroP>
+                    <Button>Travel Now</Button>
+                </HeroItems>
+            </HeroContent>
         </HeroContainer>
     )
 }
 
 export default Hero
+
+const HeroContainer = styled.div`
+    background: #0c0c0c;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    padding: 0 1rem;
+    position: relative;
+    margin-top: -80px;
+    color:  #fff;
+`
+
+const HeroBg = styled.div``
+
+const VideoBg = styled.video``
+
+const HeroContent = styled.div``
+
+const HeroItems = styled.div``
+
+const HeroH1 = styled.h1``
+
+const HeroP = styled.p``
