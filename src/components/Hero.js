c2a3ff477338700975c602d6cb6910b8e22a54d4
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-01 12:36:02 +0000

    Commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,47 +1,66 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Button } from './Button'
-import Video from '../assets/videos/bg-vid.mp4'
+import React from 'react';
+import styled from 'styled-components';
+import { Button } from './Button';
+import Video from '../assets/videos/bg-vid.mp4';
 
 const Hero = () => {
-    return (
-        <HeroContainer>
-            <HeroBg>
-                <VideoBg src={Video} type="video/mp4" autoPlay loop muted playsInline/>
-            </HeroBg>
-            <HeroContent>
-                <HeroItems>
-                    <HeroH1>Unreal Desitinatiions</HeroH1>
-                    <HeroP>Out of this world</HeroP>
-                    <Button>Travel Now</Button>
-                </HeroItems>
-            </HeroContent>
-        </HeroContainer>
-    )
-}
+  return (
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg src={Video} type='video/mp4' autoPlay loop muted playsInline />
+      </HeroBg>
+      <HeroContent>
+        <HeroItems>
+          <HeroH1>Unreal Desitinatiions</HeroH1>
+          <HeroP>Out of this world</HeroP>
+          <Button>Travel Now</Button>
+        </HeroItems>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
 
-export default Hero
+export default Hero;
 
 const HeroContainer = styled.div`
-    background: #0c0c0c;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    padding: 0 1rem;
-    position: relative;
-    margin-top: -80px;
-    color:  #fff;
-`
+  background: #0c0c0c;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  padding: 0 1rem;
+  position: relative;
+  margin-top: -80px;
+  color: #fff;
+`;
 
-const HeroBg = styled.div``
+const HeroBg = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`;
 
-const VideoBg = styled.video``
+const VideoBg = styled.video`
+  width: 100%;
+  height: 100%;
+  -o-object-fit: cover;
+  object-fit: cover;
+`;
 
-const HeroContent = styled.div``
+const HeroContent = styled.div`
+  z-index: 3;
+  height: calc(100vh - 80px);
+  max-height: 100%;
+  padding: 0rem clac((100vw- 1300px) / 2);
+`;
 
-const HeroItems = styled.div``
+const HeroItems = styled.div``;
 
-const HeroH1 = styled.h1``
+const HeroH1 = styled.h1``;
 
-const HeroP = styled.p``
+const HeroP = styled.p``;
