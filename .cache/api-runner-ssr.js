81ceb6c718c861290bb18c74790c844b2dcb4ba5
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-01 11:06:57 +0000

    Commit

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -2,6 +2,9 @@ var plugins = [{
       plugin: require('/Users/Mohamed/Projects/Travel-Website/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
     },{
+      plugin: require('/Users/Mohamed/Projects/Travel-Website/node_modules/gatsby-plugin-styled-components/gatsby-ssr'),
+      options: {"plugins":[],"displayName":true,"fileName":true,"minify":true,"transpileTemplateLiterals":true,"pure":false},
+    },{
       plugin: require('/Users/Mohamed/Projects/Travel-Website/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
       options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/gatsby-icon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"4a9773549091c227cd2eb82ccd9c5e3a"},
     }]
