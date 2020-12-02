e034b2394e9fa0eed8323dee33059b4e3619fb84
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-02 11:50:58 +0000

    Commit

--- src/components/Trips.js
+++ src/components/Trips.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
 const Trips = () => {
-    return (
-        <div>
-            <h1>Trips</h1>
-        </div>
-    )
-}
+  return (
+    <ProductsContainer>
+      <ProductsHeading></ProductsHeading>
+      <ProductWrapper></ProductWrapper>
+    </ProductsContainer>
+  );
+};
 
-export default Trips
+export default Trips;
+
+const ProductsContainer = styled.div`
+  min-height: 100vh;
+  padding: 5rem calc((100vw -1300px) / 2);
+  background: #fff;
+  color: #fff;
+`;
+
+const ProductsHeading = styled.div``;
+
+const ProductWrapper = styled.div``;
