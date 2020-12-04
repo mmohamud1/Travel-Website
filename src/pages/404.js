c79eb028fa16d22d567cfcaf01ca3ff3b6fd3f81
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-04 12:22:44 +0000

    Commit

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>Fam the website is not finished yet. Go back G!</p>
   </Layout>
 )
 
