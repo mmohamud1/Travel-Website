4b10e7f3b6ec57db55d48728f0f1c6c63ae772c5
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-04 13:45:08 +0000

    Commit

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,14 @@
-import React from "react"
+import React from 'react';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
+    <SEO title='404: Not found' />
     <h1>404: Not Found</h1>
     <p>Fam the website is not finished yet. Go back G!</p>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
