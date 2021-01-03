c9661e5fa00ca6d13e01fb18ef5f253f7245f8c9
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2021-01-03 12:53:02 +0000

    init trips styling

--- src/components/Trips.js
+++ src/components/Trips.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { graphql, useStaticQuery } from 'gatsby';
 import Img from 'gatsby-image';
 import { Button } from './Button';
+import { ImLocation } from 'react-icons/im';
 
 const Trips = () => {
   const data = useStaticQuery(graphql`
@@ -31,14 +32,14 @@ const Trips = () => {
     data.allTripsJson.edges.forEach((item, index) => {
       tripsArray.push(
         <ProductCard key={index}>
-          <Img
+          <ProductImg
             src={item.node.img.childImageSharp.fluid.src}
             alt={item.node.alt}
-            fluid={item.node.img.childImageSharp.fluid}     
+            fluid={item.node.img.childImageSharp.fluid}
           />
           <ProductInfo>
             <TextWrap>
-              <ImLoaction />
+              <ImLocation />
               <ProductTitle>{item.node.name}</ProductTitle>
             </TextWrap>
             <Button to='/trips'>{item.node.button}</Button>
@@ -73,16 +74,50 @@ const ProductsHeading = styled.div`
   color: #000;
 `;
 
-const ProductWrapper = styled.div``;
+const ProductWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 10px;
+  justify-items: center;
+  padding: 0 2rem;
+
+  @media screen and (max-width: 1200px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media screen and (max-width: 868px) {
+    grid-template-columns: 1fr;
+  }
+`;
 
 const ProductCard = styled.div`
-width: 1000px;`;
+  line-height: 2;
+  width: 100%;
+  height: 500px;
+  position: relative;
+  border-radius: 10px;
+  transition: 0.2s ease;
+`;
 
-const ProductInfo = styled.div``;
+const ProductImg = styled(Img)`
+  height: 100%;
+  max-width: 100%;
+  position: absolute;
+  border-radius: 10px;
+  filter: brightness(70%);
+  transition: 0.4s cubic-bezier(0.075, 0.82, 0.165, 1);
+
+  &:hover {
+    filter: brightness(100%)
+  }
+`
+
+
+const ProductInfo = styled.div`
+  display: flex;
+  flex-direction: 
+`;
 
 const TextWrap = styled.div``;
 
-const ImLoaction = styled.div``;
-
 const ProductTitle = styled.div``;
-
